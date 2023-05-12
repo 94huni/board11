c3fe1caaeb1ae30e20352e8afde0a694fab5fb27
taehun <94_hun_i@naver.com>
2023-05-12 23:56:57 +0900

    getUser 토큰인증 실패에 대한 테스트코드

--- board/src/test/java/com/board/board/UserTest/UserGetTest.java
+++ board/src/test/java/com/board/board/UserTest/UserGetTest.java
@@ -4,12 +4,14 @@ import com.board.board.Config.Jwt.JwtProvider;
 import com.board.board.DTO.UserDTO;
 import com.board.board.Entity.User;
 import com.board.board.Entity.UserRole;
+import com.board.board.Exception.CustomException;
 import com.board.board.Repository.UserRepository;
 import com.board.board.Service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDateTime;
@@ -56,5 +58,20 @@ public class UserGetTest {
         assertEquals(Collections.singletonList(UserRole.ROLE_USER) , userDTO.getUserRoles());
         assertEquals(user.getCreateAt(), userDTO.getCreateAt());
 
+    }
+
+    @Test
+    public void invalidTokenGetUser(){
+        String token = "token";
+        Long id = 1L;
+
+        when(jwtProvider.validateToken(token)).thenReturn(false);
+
+        CustomException exception = assertThrows(CustomException.class, () -> {
+           userService.getUser(id,token);
+        });
+
+        assertEquals("만료되었거나 토큰이 잘못됐습니다!", exception.getMessage());
+        assertEquals(HttpStatus.UNAUTHORIZED, exception.getHttpStatus());
     }
 }
